1d24d7ede4c896697538f58154c7d4da7253d102
Gamygams1234 <gamyburgos@gmail.com>
2020-06-29 17:41:57 -0700

    Built out grid and layout components

---
+++ src/components/headernav/navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <h1> nav</h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import Navbar from "./headernav/navbar";
+import Header from "./headernav/header";
 
 class Layout extends Component {
   render() {
-    return <div className="app">{...this.props.children}</div>;
+    return (
+      <div className="layout">
+        <Header />
+        <Navbar />
+        {...this.props.children}
+      </div>
+    );
   }
 }
 
