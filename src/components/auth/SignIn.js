ae42c438256bb002f918f5c0caa4ad54d6d3caf5
Gamygams1234 <gamyburgos@gmail.com>
2020-06-29 23:51:18 -0700

    Creating Page title component and added fornts

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SignInForm from "./SignInForm";
 
+import PageTitle from "../PageTitle";
 class SignIn extends Component {
   // this will handle our submission and we do not have to pass e.prevent Default
   onSubmit = (fields) => {
@@ -9,6 +10,7 @@ class SignIn extends Component {
   render() {
     return (
       <div className="sign-in">
+        <PageTitle className="sign-in__page-title" title="Login" />
         <SignInForm onSubmit={this.onSubmit} className="sign-in__form" />
       </div>
     );
