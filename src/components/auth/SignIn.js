7d5a9c109d215fcd4cc3f5794f181178f024a1e1
Gamygams1234 <gamyburgos@gmail.com>
2020-06-29 23:28:25 -0700

    Creating a Form Button Component

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 import SignInForm from "./SignInForm";
 
 class SignIn extends Component {
+  // this will handle our submission and we do not have to pass e.prevent Default
+  onSubmit = (fields) => {
+    console.log(fields);
+  };
   render() {
     return (
       <div className="sign-in">
-        <SignInForm className="sign-in__form" />
+        <SignInForm onSubmit={this.onSubmit} className="sign-in__form" />
       </div>
     );
   }
