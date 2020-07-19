5926791c216198535c61fe787768739549017001
Gamygams1234 <gamyburgos@gmail.com>
2020-07-19 14:16:02 -0700

    Creating the Purchase Detail Children Components

--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Purchases from "./purchases";
+import PurchaseDetail from "./purchaseDetail";
 
 import PageTitle from "../pageTitle";
 class PurchaseHistory extends Component {
@@ -6,6 +8,9 @@ class PurchaseHistory extends Component {
     return (
       <div className="purchase-history">
         purchase history <PageTitle className="purchase-history__page-title" title="Purchase History" />
+        <Purchases className="purchase-history__purchases" />
+        <PurchaseDetail className="purchase-history__detail" />
+        <div className="purchase-history__border-bottom"></div>
       </div>
     );
   }
