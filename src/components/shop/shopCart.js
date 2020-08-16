cd39bf2497db8dcf25a1a056abdf9f976ff8876b
Gamygams1234 <gamyburgos@gmail.com>
2020-08-16 14:49:39 -0700

    Making the shopping cart

---
+++ src/components/shop/shopCart.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class ShopCart extends Component {
+  render() {
+    const { className } = this.props;
+    return <div className={`${className} shop-cart`}>cart....</div>;
+  }
+}
+
+export default ShopCart;
