367917ca15bc90d0bd7be1eeca002c9b4b122fa5
Gamygams1234 <gamyburgos@gmail.com>
2020-08-30 14:09:16 -0700

    Built order review grid

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -6,15 +6,17 @@ import { FormButton } from "../FormFields";
 
 import history from "../../history";
 
+import ReviewProducts from "./reviewProducts";
 class ReviewForm extends Component {
   render() {
     const { className, handleSubmit } = this.props;
 
     return (
       <form onSubmit={handleSubmit} className={`${className} review-form`}>
+        <ReviewProducts className="review-form__products" />
         <div className="review-form__line"></div>
-        <Field className="review-form__proceed" onClick={() => history.push("/account")} type="submit" title="Proceed to Checkout" name="proceed" component={FormButton} />
-        <Field className="review-form__back" onClick={() => history.push("/signin")} type="button" title="Back" name="back" short={true} component={FormButton} />
+        <Field className="review-form__proceed" onClick={() => history.push("/signin")} type="submit" title="Proceed to Checkout" name="proceed" component={FormButton} />
+        <Field className="review-form__back" onClick={() => history.push("/shop")} type="button" title="Back" name="back" short={true} component={FormButton} />
       </form>
     );
   }
