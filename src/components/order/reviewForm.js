e8b95213e8e2ac4cfd65aeec14311ea468feb583
Gamygams1234 <gamyburgos@gmail.com>
2020-08-30 15:21:32 -0700

    Built subtotal for the accout

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-
 import { reduxForm, Field } from "redux-form";
-
-import { FormButton } from "../FormFields";
 
 import history from "../../history";
 
+import { FormButton } from "../FormFields";
 import ReviewProducts from "./reviewProducts";
+
 class ReviewForm extends Component {
   render() {
     const { className, handleSubmit } = this.props;
@@ -22,6 +21,24 @@ class ReviewForm extends Component {
         <div className="review-form__line"></div>
         <Field className="review-form__proceed" onClick={() => history.push("/signin")} type="submit" title="Proceed to Checkout" name="proceed" component={FormButton} />
         <Field className="review-form__back" onClick={() => history.push("/shop")} type="button" title="Back" name="back" short={true} component={FormButton} />
+        <div className="review-form__details review-details">
+          <div className="review-details__subtotal review-detail">
+            <div className="review-detail__title">Subtotal</div>
+            <div className="review-detail__price">$7.96</div>
+          </div>
+          <div className="review-details__subtotal review-detail">
+            <div className="review-detail__title">Tax</div>
+            <div className="review-detail__price">$0.16</div>
+          </div>
+          <div className="review-details__subtotal review-detail">
+            <div className="review-detail__title">Shipping</div>
+            <div className="review-detail__price">$0.00</div>
+          </div>
+          <div className="review-details__total review-detail review-detail-green">
+            <div className="review-detail__title review-detail-green__title">Total</div>
+            <div className="review-detail__price review-detail-green__price">$8.12</div>
+          </div>
+        </div>
       </form>
     );
   }
