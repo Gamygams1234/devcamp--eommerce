7f439a4e5aec385f9beaab2ca84ed8bc44ff97d8
Gamygams1234 <gamyburgos@gmail.com>
2020-09-06 12:22:06 -0700

    JSX Structure for the shipping form

--- src/components/information/payment.js
+++ src/components/information/payment.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 
 import PageTitle from "../pageTitle";
+import React, { Component } from "react";
+
+import PageTitle from "../pageTitle";
 
 // REDUX
 
 import { connect } from "react-redux";
 import * as actions from "../../actions";
+
+import PaymentForm from "./paymentForm";
 
 class Payment extends Component {
   componentDidMount() {
@@ -21,7 +26,7 @@ class Payment extends Component {
     return (
       <div className="sign-in">
         <PageTitle className="sign-in__page-title" title="Payment Information" />
-        {/* <PaymentForm onSubmit={this.onSubmit} className='sign-in__form' /> */}
+        <PaymentForm onSubmit={this.onSubmit} className="sign-in__form" />
       </div>
     );
   }
