7fdf99768bb92f2bde1a36ef06d43c4231c3c92c
Gamygams1234 <gamyburgos@gmail.com>
2020-09-06 13:27:14 -0700

    Finished shop form component and sub components

--- src/components/information/shippingForm.js
+++ src/components/information/shippingForm.js
@@ -21,7 +21,7 @@ class ShippingForm extends Component {
         <div className="shipping-form__line"></div>
         <Field className="shipping-form__use-this-address" onClick={() => history.push("/information/payment")} type="submit" title="Use This Address" name="use-this-address" component={FormButton} />
         <Field className="shipping-form__back" onClick={() => history.push("/signin")} type="button" title="Back" name="back" short={true} component={FormButton} />
-        <OrderSummary className="shipping-form__summary" />
+        <OrderSummary className="shipping-form__order-summary" />
       </form>
     );
   }
